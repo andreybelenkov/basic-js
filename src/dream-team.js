d3753c2af24b2b336dfa932eab5f66cef8d0b9b9
Belenkov Andrey <lampa1991@gmail.com>
2020-10-26 08:06:39 +0300

    feat: add function tu basic js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let teamName = '';
+    if (Array.isArray(members)) {
+      for (let i = 0; i < members.length; i++) {
+        if (typeof(members[i]) === 'string') {
+          let j = 0;
+          let slicedName = members[i]; 
+          while (members[i][j] === ' ') {
+            slicedName =  slicedName.slice(1);
+            j++;
+          }
+          teamName += slicedName[0];
+        } else {
+          continue;
+        }      
+      }
+    } else {
+      return false;
+    }
+    teamNameSorted = teamName.toUpperCase().split('').sort().join('');
+    return teamNameSorted;
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) === '[object Date]') {
+    let Date = date;
+    let numMonth = Date.getMonth();
+    if ( numMonth >= 0 && numMonth <= 1 || numMonth === 11) {
+        return 'winter';
+    } else if ( numMonth >= 2 && numMonth <= 4 ) {
+        return 'spring';
+    } else if ( numMonth >= 5 && numMonth <= 7 ) {
+        return 'summer';
+    } else if ( numMonth >= 8 && numMonth <= 10 ) {
+        return 'autumn';
+    }
+  } else {    
+    throw new Error();
+  }
+}
